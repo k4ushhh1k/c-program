a7a799661fce5274b50f177873add5db3248fbac
k4ushhh1k <kaushiksahoo00@gmail.com>
2025-07-07 21:24:47 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -10,18 +10,13 @@ Code, Compile, Run and Debug online from anywhere in world.
 
 int main()
 {
-    float maths, science, history, computer, percentage;
-    printf("Enter the marks of maths =");
-    scanf("%f",&maths);
-    printf("\n Enter the marks of science =");
-    scanf("%f",&science);
-    printf("\n Enter the marks of history =");
-    scanf("%f",&history);
-    printf("\n Enter the marks of computer =");
-    scanf("%f",&computer);
-    percentage = 100*(maths + science + history + computer/4*40);
-    printf("\n The percentage of 4 subjects out of 40 marks is :%f",percentage);
-    
+    float r, pi, a;
+    printf("Enter the radius r =\n");
+    scanf("%f",&r);
+    printf("Enter the value of pi =\n");
+    scanf("%f",&pi);
+    a = pi * r * r;
+    printf("\nThe area of the cirle is : %f",a);
 
     return 0;
 }
